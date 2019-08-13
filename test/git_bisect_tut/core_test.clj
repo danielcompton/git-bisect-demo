23e11c8cf81e6e7a08b98b0298c95ef22f28f935
Daniel Compton <desk@danielcompton.net>
2019-08-13 15:15:18 +1200

    Remove redundant test

--- test/git_bisect_tut/core_test.clj
+++ test/git_bisect_tut/core_test.clj
@@ -2,9 +2,5 @@
   (:require [clojure.test :refer :all]
             [git-bisect-tut.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
-
 (deftest adder-test
   (is (= (adder 5 6) 11)))
